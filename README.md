e12e958e372676c178c5ec88dafb7e94b259cdef
Steve Poole <gruff.wizard@yahoo.com>
2021-10-11 08:19:08 +0100

    set top project to 13 as thats all PMD supports

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # A simple java project to exercise the PMD static analyser
+
+As of now 11/10/21 PMD supports a maxium Java version of 13  
---
+++ pmd-java13/src/main/java/com/devsectimes/pmddemo/java13/HelloJava13.java
@@ -0,0 +1,11 @@
+package com.devsectimes.pmddemo.java13;
+
+public class HelloJava13 {
+
+  public static void main(String[] args ) {
+
+      Point p=new Point(10,20);
+
+      System.out.printf("Hello World (PMD Java 13) Get the %s\n",p);
+  }
+}
