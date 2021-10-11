f13fc221aab91fe8e6db0327198827bc121cd0cd
Steve Poole <gruff.wizard@yahoo.com>
2021-10-11 09:49:19 +0100

    lots of changes to get maven site to work and to get PMD to output something

---
+++ pmd-java8/src/main/java/com/devsectimes/pmddemo/java8/Ct.java
@@ -0,0 +1,10 @@
+package com.devsectimes.pmddemo.java8;
+public class Ct {
+
+    public int d(int a, int b) {
+        if (b == 0)
+            return Integer.MAX_VALUE;
+        else
+            return a / b;
+    }
+}
